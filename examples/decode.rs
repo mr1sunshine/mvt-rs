a40b124f57f27014486e1f6c0990866d7d53697d
Aleksandr Ovchinnikov <aleksandr.ovchinnikov@mapbox.com>
2020-02-14 10:38:22 +0200

    Change example to accept mvt as program parameter

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,6 +1,15 @@
 use rust_mvt::decode::decode;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
 
 fn main() {
-    let arr = vec![1,2,3];
-    decode(&arr);
+    let args: Vec<String> = env::args().collect();
+
+    let mut f = File::open(&args[1]).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+
+    let tile = decode(&buffer);
+    println!("{:?}", tile.layers);
 }
