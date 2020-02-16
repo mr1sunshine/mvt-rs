b26c48780b4b9a0470dafc95edb03783e7a5b23b
Aleksandr Ovchinnikov <aleksandr.ovchinnikov@mapbox.com>
2020-02-16 19:11:27 +0200

    Add dependency to https://github.com/mapbox/mvt-fixtures

--- build.rs
+++ build.rs
@@ -1,8 +1,18 @@
 extern crate protoc_rust;
 
 use protoc_rust::Customize;
+use git2::Repository;
+use std::path::Path;
 
 fn main() {
+    if !Path::new("fixtures").exists() {
+        let url = "https://github.com/mapbox/mvt-fixtures";
+        match Repository::clone(url, "fixtures") {
+            Err(e) => panic!("failed to clone: {}", e),
+            _ => ()
+        };
+    }
+
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
         input: &["src/protos/vector_tile.proto"],
