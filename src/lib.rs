a032decaa48710a18b83380a63f5266bb060f5e7
Aleksandr Ovchinnikov <aleksandr.ovchinnikov@mapbox.com>
2020-06-05 23:48:59 +0300

    Export Command enum from the library

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub use feature::FeatureWithJson as FeatureWithJson;
 pub use feature::FeatureWithCommands as FeatureWithCommands;
 pub use feature::FeatureWithCoordinates as FeatureWithCoordinates;
 pub use feature::Geometry as Geometry;
+pub use feature::Command as Command;
 pub use feature::Polygon as Polygon;
 pub use layer::Layer as Layer;
 pub use tile::Tile as Tile;
